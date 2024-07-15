32635d2ed4d89521ac49302416663a7241ca7cd6
lunajin <luna7698@gmail.com>
2024-07-15 23:50:03 +0900

    커밋

---
+++ src/lectures/08_v-if/03_App.vue
@@ -0,0 +1,21 @@
+<template>
+	<div>
+		<h1 v-show="ok">Title입니다.</h1>
+		<button v-on:click="ok = !ok">toggle</button>
+	</div>
+</template>
+
+<script>
+import { ref } from 'vue';
+export default {
+	setup () {
+		const ok = ref(true);
+
+		return { ok }
+	}
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
